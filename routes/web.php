6b92e0de1b0ced96a2201e47f5a9c0d9e7f3b9c5
Koi <nguyenduonggit299@gmail.com>
2024-12-27 12:43:24 +0700

    auto build
    auth,regis,error...

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,9 @@ use App\Http\Controllers\ProductsController;
 use App\Http\Controllers\CategoriesController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AdminController;
+
 
 Route::resource('products', ProductsController::class);
 Route::resource('categories', CategoriesController::class);
@@ -23,3 +26,16 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__ . '/auth.php';
+
+
+Route::get('admin/dashboard', [HomeController::class, 'index'])->middleware(['auth', 'admin']);
+Route::get('view_category', [AdminController::class, 'view_category'])->middleware(['auth', 'admin']);
+Route::post('add_category', [AdminController::class, 'add_category'])->middleware(['auth', 'admin']);
+
+Route::get('delete_category/{id}', [AdminController::class, 'delete_category'])->middleware(['auth', 'admin']);
+Route::get('edit_category/{id}', [AdminController::class, 'edit_category'])->middleware(['auth', 'admin']);
+Route::post('update_category/{id}', [AdminController::class, 'update_category'])->middleware(['auth', 'admin']);
+
+Route::get('add_product', [AdminController::class, 'add_product'])->middleware(['auth', 'admin']);
+Route::post('upload_product', [AdminController::class, 'upload_product'])->middleware(['auth', 'admin']);
+Route::get('view_product', [AdminController::class, 'view_product'])->middleware(['auth', 'admin']);
